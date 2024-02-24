6a72c27124978994e011d97d8eb99feba2df1562
TYUNKA <80307830+TYUNKA@users.noreply.github.com>
2024-02-24 08:40:19 +0000

    Add files via upload

---
+++ Calculator.h
@@ -0,0 +1,50 @@
+//Calculator.h header file
+#include<stdio.h>
+#include<stdbool.h>
+bool validOperatorCheck(char testOperator){
+  switch(testOperator){
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+      return 1;
+    default:
+      return '\0';
+  }
+}
+int ofOperation(int num1, int num2, char operator){
+    switch(operator){
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+      case '%':
+        return num1 % num2;
+      default:
+        return '\0';
+    }
+}
+int inputOfInt(int* i, char* c){
+  while(scanf("%d%c", i, c) != 2 || (*c) != '\n'){
+    printf("Invalid input! Enter an integer please: ");
+    while (((*c) = getchar()) != EOF && (*c) != '\n');
+  }
+  return *i;
+}
+char ifOperatorNotValid(){
+  char userInput; // supposed to equal an operator (+ - * / %)
+  int ch;
+  printf("Enter the operator: ");
+  while((ch = getchar()) != '\n' && ch != EOF){};
+  while(scanf(" %c", &userInput) != 1){
+    printf("Too long. Enter in an actual operator: ");
+    scanf("%c", &userInput);
+  }
+  return userInput;
+}
+
---
+++ calculator.c
@@ -0,0 +1,27 @@
+// A calculator program
+#include<stdio.h>
+#include "Calculator.h"
+int main(){
+  printf("************\nSimple Calculator Program\n************");
+  int firstNumber, secondNumber = 0;
+  int tempFirst = 0;
+  char c1, c2; // for use in the verification of ints 1 and 2 respectively
+  char inputOperator;
+  for(;;){
+    printf("Welcome to the program!\n");
+    printf("Enter the first number: ");
+    inputOfInt(&firstNumber, &c1);
+    printf("Enter the operator: ");
+    scanf(" %c", &inputOperator);
+    while(!validOperatorCheck(inputOperator)){
+      inputOperator = ifOperatorNotValid();
+    }
+    int result;
+    printf("Enter the second number: ");
+    inputOfInt(&secondNumber, &c2);
+    result = ofOperation(firstNumber, secondNumber, inputOperator);
+    printf("The result is %d.\n", result);
+  }
+  return 0;
+}
+
